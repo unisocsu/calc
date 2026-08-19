931bc351b7493a791f0a5739d6ad66bcf8726358
Your Name <your_email@example.com>
2026-08-19 11:48:51 +0300

    setup-workflow

---
+++ calc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int one;
+int thu;
+char op;
+int result;
+int main()
+{
+	printf ("בבקשה הזן את המספר הראשון/pleas enter this one namber");
+scanf (" %d", &one);
+printf ("הזן את האופרטור/pleas enter this operator");
+scanf (" %c", &op);
+printf ("בבקשה הזן את המספר השני/pleas enter this second number");
+scanf (" %d", &thu);
+if (op == '+') result = one + thu;
+else
+	if (op == '*') result = one * thu;
+if (op == '-') result = one - thu;
+if (op == '/') result = one / thu;
+printf ("%d", result);
+
+return 0;
+}
